96ee52689942cbc60bac2dc21f1b60a5a37534c1
Alexander Fronkin <alexander.fronkin@afronkin.net>
2014-02-10 18:08:30 +0400

    Windows build updated

--- build/windows/make.bat
+++ build/windows/make.bat
@@ -1,7 +1,5 @@
 @echo off
 if not defined ACLSDK (echo ACLSDK must be defined & exit 2)
 
-call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86 2> nul:
-call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86 2> nul:
-
+call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.Cmd" /Release /x64 /2003 > nul:
 nmake /f Makefile %*
